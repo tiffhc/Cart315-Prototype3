0871b050d1ac8252d21b60eea38317539f41184d
tiffhc <46658494+tiffhc@users.noreply.github.com>
2020-02-18 20:00:11 -0500

    Push for prototype

---
+++ Assets/AICatch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AI; 
+
+public class AICatch : MonoBehaviour
+{
+    public GameObject destination; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<NavMeshAgent>().SetDestination(destination.transform.position); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/KillGame.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player; 
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(other.gameObject);
+        }
+
+        if(other.gameObject.tag == "AI")
+        {
+            Destroy(player.gameObject); 
+        }
+
+    }
+}
